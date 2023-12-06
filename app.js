a3a013902f7e8b280ac973744060ed5bd2aaf33f
Pavel <50367039+Alchi-Alchi@users.noreply.github.com>
2023-12-06 22:33:10 +0300

    addNodeModules

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const http = require ('http');
+const fs = require ('fs');
+const path = require ('path');
 const port = 3000;
 const server = http.createServer ((req, res) => {
-    res.write ('Hello User');
-    res.end ();
+    res.setHeader ('Content-Type', 'text/html');
+    let createPath = (page) => path.resolve(__dirname, 'pages', `${page}.html`);
+    let basePath = '';
+    switch (req.url) {
+        case '/':
+            basePath = createPath('auth');
+            break;
+        default:
+            basePath = createPath('error');
+            break;
+    }
+    fs.readFile(basePath, (err, data) => {
+        if (err) {
+            console.log (err);
+            res.end();
+        }
+        else {
+            res.write (data);
+            res.end();
+        }
+    })
 });
 server.listen (port, 'localhost', (error) => {
     error ? console.log (error): console.log (`listening ${port}`);
