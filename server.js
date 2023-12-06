d730e61907a8f3d7c221a5f715576cf296025af6
Pavel <50367039+Alchi-Alchi@users.noreply.github.com>
2023-12-07 02:28:00 +0300

    addMongoDB

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require ('path');
+const mongoose = require('mongoose');
+const app = express();
+const morgan = require('morgan');
+app.set('view engine', 'ejs');
+const port = 3000;
+const db = 'mongodb+srv://Pavel:ThIsAdMiN@atlascluster.kei9u77.mongodb.net/localServer?retryWrites=true&w=majority';
+mongoose
+.connect(db)
+.then((res) => console.log('Connected to DB'))
+.catch((error) => console.log(error));
+let createPath = (page) => path.resolve(__dirname, 'ejs-pages', `${page}.ejs`);
+app.listen (port, 'localhost', (error) => {
+    error ? console.log (error): console.log (`listening ${port}`);
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(express.static('styles'));
+app.get('/', (req, res) => {
+    res.render(createPath('auth'));
+});
+app.use((req, res) => {
+    res.render(createPath('error'));
+});
